a68b3765ce5e162c2d45b67c0e6f2f4636fd9fcb
Ivan mendez <mendezivan604@gmail.com>
2026-03-07 20:36:16 -0600

    Switch terminado y programa culminado

--- SiquinaMendez_Ejercicio1/Program.cs
+++ SiquinaMendez_Ejercicio1/Program.cs
@@ -50,3 +50,85 @@ if (presion < 50 || presion > 250)
     Console.WriteLine("Error: presión fuera de rango.");
 }
 
+int prioridad = 4;
+string accion = "Consulta normal";
+
+switch (tipoAtencion)
+{
+    case 1:
+        if (oxigeno < 90)
+        {
+            prioridad = 1;
+            accion = "Reanimación inmediata";
+        }
+        else
+        {
+            if (temperatura >= 39 && dolor >= 8)
+            {
+                prioridad = 2;
+                accion = "Atención urgente";
+            }
+            else
+            {
+                prioridad = 3;
+                accion = "Observación";
+            }
+        }
+        break;
+
+    case 2: 
+        if (dolor >= 7)
+        {
+            prioridad = 3;
+            accion = "Evaluación médica rápida";
+        }
+        else
+        {
+            prioridad = 4;
+            accion = "Consulta normal";
+        }
+        break;
+
+    case 3:
+        if (edad < 12)
+        {
+            if (temperatura >= 38.5)
+            {
+                prioridad = 2;
+                accion = "Atención pediátrica urgente";
+            }
+            else
+            {
+                prioridad = 3;
+                accion = "Observación pediátrica";
+            }
+        }
+        else
+        {
+            prioridad = 4;
+            accion = "Consulta pediátrica normal";
+        }
+        break;
+
+    case 4:
+        if (dolor >= 8)
+        {
+            prioridad = 2;
+            accion = "Evaluación traumatológica urgente";
+        }
+        else
+        {
+            prioridad = 3;
+            accion = "Observación";
+        }
+        break;
+
+    default:
+        Console.WriteLine("Tipo de atención inválido.");
+        break;
+}
+
+Console.WriteLine("RESULTADOS");
+Console.WriteLine("Prioridad final: " + prioridad);
+Console.WriteLine("Acción sugerida: " + accion);
+
